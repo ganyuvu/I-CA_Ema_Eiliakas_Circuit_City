1447657ec04f7b0f1bd859522e017934192e4d81
Ema Eiliakas <eeiliakas@gmail.com>
2025-01-07 18:11:07 +0000

    🚧 Changes player rotation logic
    Updated the player rotation to smoothly align with the movement direction on the Y-axis only. The fix also prevents unnecessary rotation when the player is close to the target as before they would start facing up on standstill.

--- Assets/I-CA_Ema_Eiliakas/Third Party Assets/Scripts/playerController.cs
+++ Assets/I-CA_Ema_Eiliakas/Third Party Assets/Scripts/playerController.cs
@@ -60,8 +60,13 @@ public class PlayerController : MonoBehaviour
     void FaceTarget()
 	{
         Vector3 direction = (agent.destination - transform.position).normalized;
-        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookRotationSpeed);
+
+        //Calculate the desired look rotation, but focus only on the Y-axis
+        if (direction.sqrMagnitude > 0.01f) //Ensure direction is significant enough to avoid jitter
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * lookRotationSpeed);
+        }
 	}
 
     void SetAnimations()
