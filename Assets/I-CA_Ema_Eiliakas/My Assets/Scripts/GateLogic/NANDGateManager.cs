b8ce3fef944f39b73a039d38c05e543e67ba3f64
Ema Eiliakas <eeiliakas@gmail.com>
2025-01-08 21:03:10 +0000

    ✨ Create NAND gate script
    Opposite of AND gate where there has to be nothing on both pressure plates to open the gate.

---
+++ Assets/I-CA_Ema_Eiliakas/My Assets/Scripts/GateLogic/NANDGateManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class NANDGateManager : MonoBehaviour
+{
+    [SerializeField] private CheckCollision pressurePlate1;//Reference to CheckCollision script of pressure plate 1
+    [SerializeField] private CheckCollision pressurePlate2;//Reference to CheckCollision script of pressure plate 2
+    [SerializeField] private OpenGate gateController;//Reference to OpenGate script
+
+    private bool hasPlayedSound = false;//Bool to check if the SFX has been played
+
+    AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+
+    void Update()
+    {
+        //Perform AND logic check
+        //Both pressure plates must be pressed to activate the gate
+        bool isGateActivated = !pressurePlate1.isColliding && !pressurePlate2.isColliding;
+
+        //Pass the status to OpenGate script
+        gateController.GateActivated = isGateActivated;
+
+        if (isGateActivated && !hasPlayedSound)
+        {
+            PlayOpenGateSFX();
+            hasPlayedSound = true;
+        }
+        else if (!isGateActivated && hasPlayedSound)
+        {
+            //Reset SFX bool when the gate is closed
+            hasPlayedSound = false;
+        }
+    }
+
+    //Method to play SFX
+    private void PlayOpenGateSFX()
+    {
+        audioManager.PlaySFX(audioManager.gateOpen);
+    }
+}
