8e12d07e77d53a107d8c72c175bff478f0de3701
Ema Eiliakas <eeiliakas@gmail.com>
2025-01-08 18:51:12 +0000

    🐛 Fixed collision bug with pressure plates
    The pressure plates would accidentally set the bool for checking collisions off.
    
    This would happen if multiple objects where present on the pressure plate, then when one would be removed, the bool would be incorrectly set.
    
    We just count the amount of collisions now and only remove it when there is 0.

--- Assets/I-CA_Ema_Eiliakas/My Assets/Scripts/PressurePlates/CheckCollisions.cs
+++ Assets/I-CA_Ema_Eiliakas/My Assets/Scripts/PressurePlates/CheckCollisions.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class CheckCollision : MonoBehaviour
 {
-    public bool isColliding; //Bool to check if something is colliding
+    public bool isColliding;//Bool to check if something is colliding
+    private int collisionCount = 0;//Counter to track how many objects are colliding
 
     private void OnCollisionEnter(Collision collision)
     {
         //Check if the object that collided is the player or a box
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Box"))
         {
-            isColliding = true;
-            Debug.Log("Pressure Plate is being pressed");
+            //Increment counter for each new collision
+            //This is so that if multiple objects are on a pressure plate and one object gets taken off
+            //It doesn't accidentally set the bool to false
+            collisionCount++;
+            if (collisionCount > 0)
+            {
+                //Set to true when at least one object is colliding
+                isColliding = true;
+                Debug.Log("Pressure Plate is being pressed");
+            }
         }
     }
 
@@ -19,8 +28,14 @@ public class CheckCollision : MonoBehaviour
         //When the object stops colliding
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Box"))
         {
-            isColliding = false;
-            Debug.Log("Pressure Plate is released");
+            //Decrement counter when an object exits the collision
+            collisionCount--; 
+            if (collisionCount <= 0)
+            {
+                //Set to false when no objects are colliding
+                isColliding = false;
+                Debug.Log("Pressure Plate is released");
+            }
         }
     }
 }
