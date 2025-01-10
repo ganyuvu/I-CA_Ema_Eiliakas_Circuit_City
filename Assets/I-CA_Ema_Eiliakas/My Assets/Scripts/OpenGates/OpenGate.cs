6856d869f6edb3755e73ce580a4339d8ee6a4836
Ema Eiliakas <eeiliakas@gmail.com>
2025-01-10 16:13:27 +0000

    🚧  Updated Gate Script
    Now the gate checks if the object is of layer gate or trapdoor.
    
    This will either open it vertically or horizontally depending on which layer it is.
    
    This is for some of the later game puzzles.

--- Assets/I-CA_Ema_Eiliakas/My Assets/Scripts/OpenGates/OpenGate.cs
+++ Assets/I-CA_Ema_Eiliakas/My Assets/Scripts/OpenGates/OpenGate.cs
@@ -5,6 +5,7 @@ public class OpenGate : MonoBehaviour
     [SerializeField] public bool GateActivated;//Bool to check if the gate has been activated
     [SerializeField] private Transform gate;//The gate GameObject to move
     [SerializeField] private float targetYPosition = 5f;//Desired Y position for the gate
+    [SerializeField] private float targetXPosition = 5f; //Desired X position for the trapdoor
     [SerializeField] private float speed = 2f;//Speed of gate movement
 
     private Vector3 initialPosition;//Initial position of the gate
@@ -17,7 +18,22 @@ public class OpenGate : MonoBehaviour
 
     void Update()
     {
-        //Check if the gate should rise based on the GateActivated status
+        //Check which layer the gate is on
+        if (gate.gameObject.layer == LayerMask.NameToLayer("Gate"))
+        {
+            //Handle movement for regular gate
+            OpenGateVertically();
+        }
+        else if (gate.gameObject.layer == LayerMask.NameToLayer("TrapDoor"))
+        {
+            //Handle movement for trapdoor
+            OpenTrapDoorHorizontally();
+        }
+    }
+
+     //Method for opening a gate 
+    private void OpenGateVertically()
+    {
         if (GateActivated)
         {
             Vector3 targetPosition = new Vector3(initialPosition.x, targetYPosition, initialPosition.z);
@@ -27,6 +43,22 @@ public class OpenGate : MonoBehaviour
         else
         {
             //Smoothly return the gate to its initial position
+            gate.position = Vector3.Lerp(gate.position, initialPosition, Time.deltaTime * speed);
+        }
+    }
+
+    //Method for opening a trapdoor
+    private void OpenTrapDoorHorizontally()
+    {
+        if (GateActivated)
+        {
+            Vector3 targetPosition = new Vector3(targetXPosition, initialPosition.y, initialPosition.z);
+            //Smoothly move the trapdoor to the target position
+            gate.position = Vector3.Lerp(gate.position, targetPosition, Time.deltaTime * speed);
+        }
+        else
+        {
+            //Smoothly return the trapdoor to its initial position
             gate.position = Vector3.Lerp(gate.position, initialPosition, Time.deltaTime * speed);
         }
     }
